6c2f6e6add4edcafefb4c0766625d94d19797822
sainteckes <stephan@neodsp.com>
2024-11-25 11:43:49 +0100

    add library for glibc 2.31

--- crates/rtsan-sys/build.rs
+++ crates/rtsan-sys/build.rs
@@ -1,7 +1,9 @@
 fn main() {
+    let target = std::env::var("TARGET").expect("Could not find target");
+    println!("Target triple: {}", target);
     // Specify the directory where the `.a` library is located
     let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-    println!("cargo:rustc-link-search=native={}/lib", manifest_dir);
+    println!("cargo:rustc-link-search=native={manifest_dir}/lib/{target}",);
 
     // Specify the library to link with (omit the `lib` prefix and `.a` extension)
     println!("cargo:rustc-link-lib=static=clang_rt.rtsan");
