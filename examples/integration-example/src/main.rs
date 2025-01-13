35b770969d51d779a05cdc4b86d373d4756789a3
steckes <stephan@neodsp.com>
2025-01-13 14:26:02 +0100

    update integration example

--- examples/integration-example/src/main.rs
+++ examples/integration-example/src/main.rs
@@ -1,19 +1,16 @@
 use rtsan_standalone::*;
 
-#[derive(Default)]
-pub struct MyProcessor {}
-
-impl MyProcessor {
-    /// Add the [`rtsan::nonblocking`] macro to the process function.
-    /// In case the default-feature `sanitize` is not provided,
-    /// this macro won't do anything, so it can stay in production code.
-    #[nonblocking]
-    pub fn process(&mut self, audio: &mut [f32]) {
+/// Add the [`rtsan::nonblocking`] macro to the process function.
+/// In case the default-feature `sanitize` is not provided,
+/// this macro won't do anything, so it can stay in production code.
+#[nonblocking]
+fn process(audio: &mut [f32]) {
+    scoped_disabler! {
         assert_eq!(audio.len(), 256); // wrong assertions and panics will trigger the sanitizer before the panic message is printed!
-        let data = vec![2.0; 256]; // oops!
-        for (output, input) in audio.iter_mut().zip(data) {
-            *output *= input;
-        }
+    }
+    let data = vec![2.0; 256]; // oops!
+    for (output, input) in audio.iter_mut().zip(data) {
+        *output *= input;
     }
 }
 
@@ -21,8 +18,8 @@ fn main() {
     // call this always at the start of your program
     ensure_initialized();
 
-    let mut processor = MyProcessor::default();
-
     let mut audio = vec![1.0; 256];
-    processor.process(&mut audio);
+    process(&mut audio);
+
+    println!("Example finished without sanitizing!");
 }
