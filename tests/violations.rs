e9d9c3a892e7dfe55f52e0ee83813d9884841194
luca3s <41015240+luca3s@users.noreply.github.com>
2025-01-25 12:40:02 +0100

    regex supprt

--- tests/violations.rs
+++ tests/violations.rs
@@ -1,6 +1,7 @@
 extern crate libtest_mimic;
 
 use libtest_mimic::{Arguments, Failed, Trial};
+use regex::bytes::Regex;
 use std::{
     fs::read_dir,
     process::{Command, ExitCode, Stdio},
@@ -9,23 +10,26 @@ use std::{
 fn main() -> ExitCode {
     let args = Arguments::from_args();
 
-    let mut test_cases = Vec::new();
+    let mut test_cases: Vec<(String, Regex)> = Vec::new();
 
     // collect test cases
     for file in read_dir("detection-tests/examples").unwrap() {
         let file = file.unwrap();
         println!("{:?}", file);
         assert!(file.metadata().unwrap().is_file());
+        let content = std::fs::read_to_string(file.path()).unwrap();
+        let first_line = content.lines().next().unwrap().strip_prefix("// ").unwrap();
+        let regex = Regex::new(first_line).unwrap();
 
-        test_cases.push(file.file_name().into_string().unwrap().strip_suffix(".rs").unwrap().to_string());
+        test_cases.push((file.file_name().into_string().unwrap().strip_suffix(".rs").unwrap().to_string(), regex));
     }
 
     println!("{test_cases:?}");
     // create tests
     let tests = test_cases
         .into_iter()
-        .map(|name| {
-            Trial::test(name.clone(), || {
+        .map(|(name, regex)| {
+            Trial::test(name.clone(), move || {
                 let process = Command::new("cargo")
                     .args([
                         "run".to_owned(),
@@ -43,9 +47,11 @@ fn main() -> ExitCode {
                 let output = process.wait_with_output().unwrap();
                 println!("{output:?}");
                 if output.status.success() {
-                    Err(Failed::without_message())
-                } else {
+                    Err(Failed::from("no violation detected"))
+                } else if regex.is_match(&output.stderr) {
                     Ok(())
+                } else {
+                    Err(Failed::from("stderr didn't match regex"))
                 }
             })
         })
